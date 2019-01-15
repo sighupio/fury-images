77d309535fbf13893087bd6c1148aad3a71c6e43
Philippe scorsolini <pscorso93@gmail.com>
2019-01-15 13:02:42 +0100

    enabling push for images

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ for dir in `find . -type d -maxdepth 1 -not -path '*/\.*' -not -name '.'`
 do
   cd ${dir}
   echo building ${dir}
-  make build # push
+  make build push
   echo built ${dir}
   cd ..
 done
