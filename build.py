1cc1e1d4f7fe1273c98ac06875062cbfaa6f3ee2
Philippe scorsolini <pscorso93@gmail.com>
2019-01-18 15:47:52 +0100

    pushing to quay

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import sys
 import itertools
 
 specs={}
+repo = sys.argv[1] if len(sys.argv) > 1 else ""
 for d in [ d for d in os.listdir(".") if not d.startswith(".") and os.path.isdir(d) and os.path.isfile(os.path.join(d,"spec.yaml"))]:
     with open(os.path.join(d,"spec.yaml")) as f:
         specs[d]=yaml.load(f)
@@ -13,7 +14,7 @@ print([dict(zip(specs["furyctl"]["tags"].keys(), values)) for values in itertool
 
 for k in specs.keys():
     for ts in [dict(zip(specs[k]["tags"].keys(), values)) for values in itertools.product(*specs[k]["tags"].values())]:
-        image = specs[k]["image"] + ":" + "_".join([str(ts[ts_key]) for ts_key in sorted(ts.keys())])
+        image = repo + specs[k]["image"] + ":" + "_".join([str(ts[ts_key]) for ts_key in sorted(ts.keys())])
         print("building: " + image)
         build = "docker build " + " ".join(["--build-arg " + k + "=" + str(v) for (k,v) in ts.items()]) + " -t " + image + " " + k
         print(build)
