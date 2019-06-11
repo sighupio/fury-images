9c68166d26878e714f071f36e030e4a21752e2fe
Luca Novara <luca@sighup.io>
2019-06-11 10:42:52 +0200

    switched demo-exporter to Node.js

---
+++ demo-exporter/server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var os = require('os');
+
+var totalrequests = 0;
+
+http.createServer(function(request, response) {
+  totalrequests += 1
+
+  response.writeHead(200);
+
+  if (request.url == "/metrics") {
+    response.end("# HELP transactions_count_total The amount of requests served by the server in total\n# TYPE transactions_count_total counter\ntransactions_count_total " + totalrequests + "\n");
+    return;
+  }
+  response.end("Hello! My name is " + os.hostname() + ". I have served "+ totalrequests + " requests so far.\n");
+}).listen(8080)
