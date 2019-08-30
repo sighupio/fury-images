fbec6094d5856aefca21abdedb9f186929ad06a5
phisco <pscorso93@gmail.com>
2019-08-30 17:10:52 +0200

    tentative fix

--- kind/setup.sh
+++ kind/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo "waiting for docker to start"
-let i=0; while (! docker ps ); do let i=i+1; [ $i -le 12 ] || exit 1 && sleep 5; done
+let i=0; while ! docker ps; do let i=i+1; [ $i -le 12 ] || exit 1 && sleep 5; done
 kind create cluster --name "$NAME" --config /kind-config --wait 1m --loglevel=debug
 export KUBECONFIG="$(kind get kubeconfig-path --name="$NAME")"
 sed -i -E -e 's/localhost|0\.0\.0\.0/'"$CLUSTER_HOST"'/g' "$KUBECONFIG"
