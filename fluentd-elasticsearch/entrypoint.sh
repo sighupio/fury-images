6c9c2ff88d140a148eba93d7356827e18fcd8661
Philippe scorsolini <pscorso93@gmail.com>
2019-09-26 15:45:25 +0200

    Revert "Update fluetd-elasticsearch to v1.7.2"
    This reverts commit 1fbe446ccf7b16525eb89e998a607d1d450ddbc5.

--- fluentd-elasticsearch/entrypoint.sh
+++ fluentd-elasticsearch/entrypoint.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
-
+#!/usr/bin/dumb-init /bin/sh
 
 set -e
+set -x
 
-if [ -z ${FLUENT_ELASTICSEARCH_SED_DISABLE} ] ; then
-  if [ -z ${FLUENT_ELASTICSEARCH_USER} ] ; then
-    sed -i  '/FLUENT_ELASTICSEARCH_USER/d' /fluentd/etc/${FLUENTD_CONF}
-  fi
+mkdir -p /fluentd/etc
+cp /etc/tmp-conf/* /fluentd/etc/
 
-  if [ -z ${FLUENT_ELASTICSEARCH_PASSWORD} ] ; then
-    sed -i  '/FLUENT_ELASTICSEARCH_PASSWORD/d' /fluentd/etc/${FLUENTD_CONF}
-  fi
+if [ -z ${FLUENT_ELASTICSEARCH_USER} ] ; then
+   sed -i  '/FLUENT_ELASTICSEARCH_USER/d' /fluentd/etc/${FLUENTD_CONF}
+fi
+
+if [ -z ${FLUENT_ELASTICSEARCH_PASSWORD} ] ; then
+   sed -i  '/FLUENT_ELASTICSEARCH_PASSWORD/d' /fluentd/etc/${FLUENTD_CONF}
 fi
 
 exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
