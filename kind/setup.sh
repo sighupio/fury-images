3e27c7292e6302a28c4f2f428a42b45d354f3a53
Ángel Barrera Sánchez <angel@sighup.io>
2019-11-04 12:37:26 +0100

    Add new kind container image with static kubernetes versions

--- kind/setup.sh
+++ kind/setup.sh
@@ -2,7 +2,7 @@
 echo "waiting for docker to start"
 let i=0; while ! docker ps; do let i=i+1; [ $i -le 12 ] || exit 1 && sleep 5; done || exit 1
 set -e
-kind create cluster --name "$NAME" --config /kind-config --wait 1m --loglevel=debug
+kind create cluster --name "$NAME" --config /kind-config --image kindest/node:${K8S_VERSION} --wait 1m --loglevel=debug
 export KUBECONFIG="$(kind get kubeconfig-path --name="$NAME")"
 sed -i -E -e 's/localhost|0\.0\.0\.0/'"$CLUSTER_HOST"'/g' "$KUBECONFIG"
 kubectl apply -f - <<EOF
