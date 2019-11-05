019da09fb4ad9e41e48650af3edb8aabec31f2d9
Ángel Barrera Sánchez <angel@sighup.io>
2019-11-05 13:44:07 +0100

    Changed the way kubernetes configuration is used and written

--- kind/setup.sh
+++ kind/setup.sh
@@ -3,7 +3,8 @@ echo "waiting for docker to start"
 let i=0; while ! docker ps; do let i=i+1; [ $i -le 12 ] || exit 1 && sleep 5; done || exit 1
 set -e
 kind create cluster --name "$NAME" --config /kind-config --image kindest/node:${K8S_VERSION} --wait 1m --loglevel=debug
-export KUBECONFIG="$(kind get kubeconfig-path --name="$NAME")"
+kind get kubeconfig --name "$NAME" > /kubeconfig
+export KUBECONFIG=/kubeconfig
 sed -i -E -e 's/localhost|0\.0\.0\.0/'"$CLUSTER_HOST"'/g' "$KUBECONFIG"
 kubectl apply -f - <<EOF
 ---
