41404991cf567ab6ccfafa24d0893dd4d918a9cf
Ángel Barrera Sánchez <angel@sighup.io>
2020-04-22 13:28:10 +0200

    Update fluentd-elasticsearch image

--- fluentd-elasticsearch/entrypoint.sh
+++ fluentd-elasticsearch/entrypoint.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
-set -e
 
-exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
+SIMPLE_SNIFFER=$( gem contents fluent-plugin-elasticsearch | grep elasticsearch_simple_sniffer.rb )
+
+# If the user has supplied only arguments append them to `fluentd` command
+if [ "${1#-}" != "$1" ]; then
+    set -- fluentd "$@"
+fi
+
+# If user does not supply config file or plugins, use the default
+if [ "$1" = "fluentd" ]; then
+    if ! echo $@ | grep ' \-c' ; then
+       set -- "$@" -c /fluentd/etc/${FLUENTD_CONF}
+    fi
+
+    if ! echo $@ | grep ' \-p' ; then
+       set -- "$@" -p /fluentd/plugins
+    fi
+
+    if ! echo $@ | grep ' \-r' ; then
+       set -- "$@" -r ${SIMPLE_SNIFFER}
+    fi
+fi
+
+exec "$@"
