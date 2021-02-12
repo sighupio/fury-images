55298d4b2bc0327a6108e712b88e23335ff749c5
Ángel Barrera Sánchez <angel@sighup.io>
2021-02-12 13:41:09 +0100

    Add a new check. NET_ADMIN or privileged on e2e furyctl

--- e2e-furyctl/vpn-connect.sh
+++ e2e-furyctl/vpn-connect.sh
@@ -7,15 +7,23 @@ if [ ! -f "$OVPN_PROFILE" ]; then
   exit 1
 fi
 
-capsh --print | grep "Current:" | cut -d' ' -f3 | grep -q cap_net_admin
-RESULT=$?
-if [ $RESULT -eq 1 ]; then
-  echo "ERROR: Ensure you are running the container using --cap-add=NET_ADMIN"
-  exit 1
+# If the container can add a new net interface, means it runs as privileged
+# source: https://stackoverflow.com/questions/32144575/how-to-know-if-a-docker-container-is-running-in-privileged-mode
+ip link add dummy0 type dummy >/dev/null
+if [ $? -eq 0 ]; then
+  # clean the dummy0 link
+  ip link delete dummy0 >/dev/null
+else
+  # if not, check if it has the NET_ADMIN capability is enabled.
+  capsh --print | grep "Current:" | cut -d' ' -f3 | grep -q cap_net_admin
+  if [ $? -eq 1 ]; then
+    echo "ERROR: Ensure you are running the container using --cap-add=NET_ADMIN or --privileged"
+    exit 1
+  fi
 fi
 
 mkdir -p /dev/net
-ls /dev/net/tun > /dev/null 2>&1
+ls /dev/net/tun >/dev/null 2>&1
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   mknod /dev/net/tun c 10 200
