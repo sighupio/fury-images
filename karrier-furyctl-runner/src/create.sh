7b61b66b1728c9676e0bcf8564a64f1d7facdd47
Luca Zecca <l.zecca78@gmail.com>
2022-06-10 17:31:43 +0200

    chore: missing return to previous dir

--- karrier-furyctl-runner/src/create.sh
+++ karrier-furyctl-runner/src/create.sh
@@ -288,6 +288,7 @@ if [ -d "terraform/providers/${PROVIDER_NAME}" ]; then
   terraform init
   TF_VAR_fqdn=${INGRESS_BASE_URL} TF_VAR_instance_filter=${instance_filter} terraform plan
   retry_command "TF_VAR_fqdn=${INGRESS_BASE_URL} TF_VAR_instance_filter=${instance_filter} terraform apply -auto-approve" 10 4
+  cd -
 fi
 
 # deploy provider-specific modules
