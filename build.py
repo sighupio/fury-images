b02a9a887e0d303b507c8bec7a42c063f02fb148
Claudio Beatrice <claudi0.beatric3@gmail.com>
2024-01-25 12:40:00 +0100

    fix: hoisting functions in python build script

--- build.py
+++ build.py
@@ -4,41 +4,6 @@ import yaml
 import sys
 import itertools
 
-specs = {}
-errors = []
-repo = sys.argv[1] + "/" if len(sys.argv) > 1 else ""
-modified_files = set([os.path.dirname(file) for file in sys.argv[2:] ])
-debug(modified_files)
-
-# We need to build the images for all the modified files
-for d in [d for d in os.listdir(".") if not d.startswith(".") and os.path.isdir(d) and d in modified_files and os.path.isfile(os.path.join(d, "spec.yaml"))]:
-    with open(os.path.join(d,"spec.yaml")) as f:
-        specs[d]=yaml.load(f, Loader=yaml.SafeLoader)
-debug(yaml.dump(specs))
-
-for k in specs.keys():
-    is_multiarch = "platforms" in specs[k].keys()
-
-    if is_multiarch:
-        archs = get_archs(specs[k])
-        lazy_create_builder("sighup")
-
-    # For each spec's key, we do the cartesian product of the tags, then we generate a build for each combination of tags and architectures,
-    # The result of the cartesian product is a dictionary of tags:values
-    for ts in [dict(zip(specs[k]["tags"].keys(), values)) for values in itertools.product(*specs[k]["tags"].values())]:
-        # An image result example would be: quay.io/sighup/node:20.7.0_18.7.0
-        image = repo + specs[k]["image"] + ":" + "_".join([str(ts[ts_key]) for ts_key in sorted(ts.keys())])
-        debug("building: " + image)
-
-        # Finally, we build the image
-        if is_multiarch:
-            docker_buildx(ts, image, archs, errors)
-        else:
-            docker_build(ts, image, errors)
-
-if len(errors) > 0:
-    print("Errors found: {}".format(errors))
-    sys.exit(1)
 
 def docker_build(ts, image, errors):
     build = "docker build " + " ".join(["--build-arg " + k + "=" + str(v)
@@ -94,3 +59,39 @@ def debug(arg):
     if os.environ.get("DEBUG") == "true":
         print(arg)
         sys.stdout.flush()
+
+specs = {}
+errors = []
+repo = sys.argv[1] + "/" if len(sys.argv) > 1 else ""
+modified_files = set([os.path.dirname(file) for file in sys.argv[2:] ])
+debug(modified_files)
+
+# We need to build the images for all the modified files
+for d in [d for d in os.listdir(".") if not d.startswith(".") and os.path.isdir(d) and d in modified_files and os.path.isfile(os.path.join(d, "spec.yaml"))]:
+    with open(os.path.join(d,"spec.yaml")) as f:
+        specs[d]=yaml.load(f, Loader=yaml.SafeLoader)
+debug(yaml.dump(specs))
+
+for k in specs.keys():
+    is_multiarch = "platforms" in specs[k].keys()
+
+    if is_multiarch:
+        archs = get_archs(specs[k])
+        lazy_create_builder("sighup")
+
+    # For each spec's key, we do the cartesian product of the tags, then we generate a build for each combination of tags and architectures,
+    # The result of the cartesian product is a dictionary of tags:values
+    for ts in [dict(zip(specs[k]["tags"].keys(), values)) for values in itertools.product(*specs[k]["tags"].values())]:
+        # An image result example would be: quay.io/sighup/node:20.7.0_18.7.0
+        image = repo + specs[k]["image"] + ":" + "_".join([str(ts[ts_key]) for ts_key in sorted(ts.keys())])
+        debug("building: " + image)
+
+        # Finally, we build the image
+        if is_multiarch:
+            docker_buildx(ts, image, archs, errors)
+        else:
+            docker_build(ts, image, errors)
+
+if len(errors) > 0:
+    print("Errors found: {}".format(errors))
+    sys.exit(1)
